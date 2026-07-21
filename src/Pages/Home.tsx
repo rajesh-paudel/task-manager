478cf5f21b07193ab17b2c5f973a5c14f8d00dc8
Rajesh Paudel <rajeshpaudel9863@gmail.com>
2026-07-21 17:04:07 +0545

    add open graph

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -19,6 +19,31 @@ export default function Home() {
           name="description"
           content="TaskPulse is a modern task management platform to organize projects, track progress, and boost productivity."
         />
+        <meta
+          property="og:title"
+          content="TaskPulse | Organize Tasks, Projects & Team Work"
+        />
+
+        <meta
+          property="og:description"
+          content="TaskPulse is a modern task management platform to organize projects, track progress, and boost productivity."
+        />
+
+        <meta
+          property="og:image"
+          content="https://task-manager-five-omega-36.vercel.app/og-image.png"
+        />
+
+        <meta
+          property="og:url"
+          content="https://task-manager-five-omega-36.vercel.app/"
+        />
+
+        <meta property="og:type" content="website" />
+
+        <meta property="og:site_name" content="TaskPulse" />
+
+        <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
 
       <Hero />
