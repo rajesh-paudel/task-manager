f5e8cd0394a17ead08768c16155f9b1be82c1ae3
Rajesh Paudel <rajeshpaudel9863@gmail.com>
2026-07-22 14:03:10 +0545

    test

--- src/store/store.ts
+++ src/store/store.ts
@@ -13,6 +13,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-// Custom hooks with strict TypeScript definitions
+// Custom hooks with strict TypeScript definition
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
