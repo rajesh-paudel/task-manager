485b986af1dacbb7beb40c8097f3fd0e0ca40e1a
Rajesh Paudel <rajeshpaudel9863@gmail.com>
2026-07-22 11:27:40 +0545

    add proper loading spinners in protected and public routes

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -5,7 +5,11 @@ export default function ProtectedRoute() {
   const { userProfile, loading } = useAppSelector((state) => state.auth);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="h-6 w-6 border-2 border-orange-600 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
   }
 
   return userProfile ? <Outlet /> : <Navigate to="/login" />;
--- src/components/PublicRoute.tsx
+++ src/components/PublicRoute.tsx
@@ -4,7 +4,13 @@ import { useAppSelector } from "../store/store";
 export default function PublicRoute() {
   const { userProfile, loading } = useAppSelector((state) => state.auth);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="h-6 w-6 border-2 border-orange-600 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
 
   return userProfile ? <Navigate to="/dashboard" replace /> : <Outlet />;
 }
